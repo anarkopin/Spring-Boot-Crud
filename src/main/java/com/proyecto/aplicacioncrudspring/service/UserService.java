bd27e1d484014f81aea5406998c8fdb1fba19e91
anarkopin <diebejardelaguila@gmail.com>
2022-06-10 16:04:27 -0500

    Implent "Delete user" Service Close #7

--- src/main/java/com/proyecto/aplicacioncrudspring/service/UserService.java
+++ src/main/java/com/proyecto/aplicacioncrudspring/service/UserService.java
@@ -2,10 +2,12 @@ package com.proyecto.aplicacioncrudspring.service;
 
 import com.proyecto.aplicacioncrudspring.entities.User;
 
+//spring trabaja de manera interna en procesar la implementacion a pesar de que tu llames al servicio en un primer moemento y no la implementacion
 public interface UserService{
 
     public Iterable<User> getAllUsers();
     public User createUser(User user) throws Exception;
     public User getUserById(Long id) throws Exception;
     public User updateUser(User user) throws Exception;
+    public void deteleteUser(Long id) throws Exception;
 }
